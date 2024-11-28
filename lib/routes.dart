e47a7d19406d939bed4252d756f9ae0770d618bf
NyraSama <debouvriesryan@gmail.com>
2024-11-28 14:05:29 +0100

    feat(auth_guard): Implement auth guard on router

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:xpeapp_admin/data/entities/newsletter_entity.dart';
-import 'package:xpeapp_admin/presentation/pages/access/feature_flipping_page.dart';
+import 'package:xpeapp_admin/presentation/pages/auth_guard.dart';
+import 'package:xpeapp_admin/presentation/pages/feature_flipping/feature_flipping_page.dart';
 import 'package:xpeapp_admin/presentation/pages/home_page.dart';
 import 'package:xpeapp_admin/presentation/pages/login_page.dart';
 import 'package:xpeapp_admin/presentation/pages/newsletters/newsletter_add_page.dart';
@@ -12,27 +13,40 @@ import 'package:xpeapp_admin/presentation/pages/qvst/qvst_page.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   '/': (context) => const LoginPage(),
-  '/home': (context) => const HomePage(),
-  '/newsletters': (context) => const NewslettersPage(),
-  '/newsletter/detail': (context) => NewsletterDetailPage(
-        id: ModalRoute.of(context)!.settings.arguments as String,
+  '/home': (context) => const AuthGuard(child: HomePage()),
+  '/newsletters': (context) => const AuthGuard(
+        child: NewslettersPage(),
       ),
-  '/newsletter/add': (context) => const NewsletterAddOrEditPage(
-        typePage: NewsletterTypePage.add,
+  '/newsletter/detail': (context) => AuthGuard(
+        child: NewsletterDetailPage(
+          id: ModalRoute.of(context)?.settings.arguments as String?,
+        ),
       ),
-  '/newsletter/edit': (context) => NewsletterAddOrEditPage(
-        typePage: NewsletterTypePage.edit,
-        newsletter:
-            ModalRoute.of(context)!.settings.arguments as NewsletterEntity,
+  '/newsletter/add': (context) => const AuthGuard(
+        child: NewsletterAddOrEditPage(
+          typePage: NewsletterTypePage.add,
+        ),
       ),
-  '/featureFlipping': (context) => const FeatureFlippingPage(),
-  '/qvst': (context) => const QvstPage(),
+  '/newsletter/edit': (context) => AuthGuard(
+        child: NewsletterAddOrEditPage(
+          typePage: NewsletterTypePage.edit,
+          newsletter:
+              ModalRoute.of(context)!.settings.arguments as NewsletterEntity?,
+        ),
+      ),
+  '/featureFlipping': (context) =>
+      const AuthGuard(child: FeatureFlippingPage()),
+  '/qvst': (context) => const AuthGuard(child: QvstPage()),
   '/qvst/detail': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
-    return QvstDetailPage(
-      id: args?['id'] as String? ?? '',
+    return AuthGuard(
+      child: QvstDetailPage(
+        id: args?['id'] as String? ?? '',
+      ),
     );
   },
-  '/qvst/add': (context) => const QvstAddPage(),
+  '/qvst/add': (context) => const AuthGuard(
+        child: QvstAddPage(),
+      ),
 };
