5221e3c19c1d406cdacc9acb5ae6039c93b419b3
Théo Lebègue <theo.lebegue@icloud.com>
2026-04-14 09:56:14 +0200

    fix(ideaBox): improve navigation button logic and add error handling in dialog

--- lib/presentation/pages/idea_box/content/idea_box_content_ideas.dart
+++ lib/presentation/pages/idea_box/content/idea_box_content_ideas.dart
@@ -52,24 +52,24 @@ class IdeaBoxContentIdeasState extends ConsumerState<IdeaBoxContentIdeas> {
             ),
           ),
           const SizedBox(width: 10),
-          Tooltip(
-            message: "Précédent",
-            child: FloatingActionButton(
-              onPressed: currentPage > 1
-                  ? () {
-                      setState(() {
-                        currentPage--;
-                      });
-                    }
-                  : null,
-              backgroundColor: kDefaultXpehoColor,
-              child: const Icon(
-                Icons.arrow_back,
-                color: Colors.white,
+          if (currentPage > 1) ...[
+            Tooltip(
+              message: "Précédent",
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    currentPage--;
+                  });
+                },
+                backgroundColor: kDefaultXpehoColor,
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 10),
+            const SizedBox(width: 10),
+          ],
           ref.hasNextIdeasPage(currentPage, widget.filterStatus)
               ? Tooltip(
                   message: "Suivant",
